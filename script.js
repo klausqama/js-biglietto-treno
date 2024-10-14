06d7fda75c2bf4fcf21b9a6e4441b0b422df38ea
klausqama <klaussqama@gmail.com>
2024-10-14 16:23:42 +0200

    add prompt km

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //Controllo del corretto funzionamento prima dell'esercizio//
 const name= 'klaus'
 console.log(name)
+// Inizio ex//
+// Creare un Prompt per chiedere il numero di chilometri da percorrere al passeggero
+let km = prompt("Quanti chilometri deve percorrere?")
+console.log(km)
