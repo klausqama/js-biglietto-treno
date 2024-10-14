0721f5bee16b08cf53921565d3ab598d8e91b3d6
klausqama <klaussqama@gmail.com>
2024-10-14 16:41:42 +0200

    add prompt age

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //Controllo del corretto funzionamento prima dell'esercizio//
 const name= 'klaus'
 console.log(name)
+
 // Inizio ex//
 // Creare un Prompt per chiedere il numero di chilometri da percorrere al passeggero
 let km = prompt("Quanti chilometri deve percorrere?")
 console.log(km)
+//chiedere l'età del passeggero per eventuale sconto
+let age = prompt("inserisca la sua età")
+console.log(age)
