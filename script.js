02a33cecca9be8a2d5eb1ca92da2e220f3a69f6a
klausqama <klaussqama@gmail.com>
2024-10-14 18:04:03 +0200

    add .toFixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(name)
 // Inizio ex//
 // Creare un Prompt per chiedere il numero di chilometri da percorrere al passeggero
 let Km = prompt("Quanti chilometri deve percorrere?")
-console.log(Km) // 10
+console.log(Km)
 //chiedere l'età del passeggero per eventuale sconto
 let userAge = prompt("inserisca la sua età")
 console.log(userAge)
@@ -16,11 +16,13 @@ const numberuserAge = parseInt(userAge)
 const kmPrice = 0.21
 let totalPrice = Km * kmPrice 
 // sconto del prezzo per età del 20% 
-if(age < 18){
+if(userAge < 18){
     totalPrice = totalPrice * 20 / 100
 }
 // sconto del prezzo per età del 40%
-else if (age >= 65){
+else if (userAge >= 65){
     totalPrice = totalPrice * 40/100
 }
+// risultato del prezzo finale in numeri decimali max.2
+totalPrice = totalPrice.toFixed(2)
 console.log(totalPrice)
