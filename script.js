e0e83abd93ebd94953f6401202d9695057ce220c
klausqama <klaussqama@gmail.com>
2024-10-14 17:59:10 +0200

    add parseInt

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ console.log(name)
 
 // Inizio ex//
 // Creare un Prompt per chiedere il numero di chilometri da percorrere al passeggero
-let km = prompt("Quanti chilometri deve percorrere?")
-console.log(km) // 10
+let Km = prompt("Quanti chilometri deve percorrere?")
+console.log(Km) // 10
 //chiedere l'età del passeggero per eventuale sconto
-let age = prompt("inserisca la sua età")
-console.log(age)
+let userAge = prompt("inserisca la sua età")
+console.log(userAge)
+// modificare il valore di prompt da stringa a numero
+const numberKm = parseInt(Km)
+const numberuserAge = parseInt(userAge)
 //Prezzo del biglietto al km
 const kmPrice = 0.21
-let totalPrice = km * kmPrice 
+let totalPrice = Km * kmPrice 
 // sconto del prezzo per età del 20% 
 if(age < 18){
     totalPrice = totalPrice * 20 / 100
